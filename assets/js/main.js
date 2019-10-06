aedebd45ebdd76ff4de95f9acd00f895a077f4e4
Andrei Makas <akazimirovich@stone-labs.com>
2019-10-06 11:16:17 +0300

    scroll position fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -87,7 +87,7 @@
 				
 				if (target.length) {
 					$('html,body').animate({
-						scrollTop: target.offset().top
+						scrollTop: target.offset().top - 60
 					}, 1000);
 					return false;
 				}
